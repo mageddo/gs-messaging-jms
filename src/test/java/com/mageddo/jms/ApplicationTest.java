906c49bc743ca2a514613941a3ce590df6484787
Elvis de Freitas <edigitalb@gmail.com>
2017-06-16 23:54:39 -0300

    MG-309 simplifying test

--- src/test/java/com/mageddo/jms/ApplicationTest.java
+++ src/test/java/com/mageddo/jms/ApplicationTest.java
@@ -1,15 +1,19 @@
 package com.mageddo.jms;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.jms.annotation.EnableJms;
 
 /**
  * Created by elvis on 16/06/17.
  */
-@Import(Application.class)
-@ImportResource("classpath:activemq.xml")
-@Configuration
-public class ApplicationTest {
 
-}
+@EnableJms
+@ImportResource("classpath:activemq.xml")
+@SpringBootApplication
+@EnableAutoConfiguration
+@Configuration
+public class ApplicationTest {}
