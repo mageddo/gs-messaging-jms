2c59be30e6d2208b1c7f284390b4018cd5b42ebd
Elvis de Freitas <edigitalb@gmail.com>
2017-06-16 23:55:35 -0300

    MG-309 simplifying test

--- src/test/java/com/mageddo/jms/ApplicationTest.java
+++ src/test/java/com/mageddo/jms/ApplicationTest.java
@@ -1,6 +1,5 @@
 package com.mageddo.jms;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
